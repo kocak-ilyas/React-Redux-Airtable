efc62329459c62a3338ef805fd66029b1158d077
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-01-26 12:44:23 +0300

    footer Link

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Row } from "reactstrap";
+import { Link } from "react-router-dom";
 
 import appStore from "../../image/appStore.jpg";
 import playStore from "../../image/playStore.jpg";
