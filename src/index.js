d0a5c8508c5c2b1f6d47cf26af68bbf7b91d846d
kocak-ilyas <62467305+kocak-ilyas@users.noreply.github.com>
2021-02-16 12:14:03 +0300

    background

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/root/App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/styles.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "alertifyjs/build/css/alertify.min.css";
+import App from "./components/root/App";
 import configureStore from "./redux/reducers/configureStore";
 const store = configureStore();
 
